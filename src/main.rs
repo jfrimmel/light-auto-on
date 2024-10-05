17bbf4b4dd898f50640baa8b580ec6cb9498c133
Julian Frimmel <julian@fri-me.de>
2024-10-05 18:48:51 +0200

    Add minimal test program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#![no_std]
+#![no_main]
+
+#[no_mangle]
 fn main() {
-    println!("Hello, world!");
+    let peripherals = unsafe { avr_device::attiny85::Peripherals::steal() };
+    peripherals
+        .PORTB
+        .ddrb
+        .modify(|_r, w| w.pb3().set_bit().pb4().set_bit());
+    peripherals
+        .PORTB
+        .portb
+        .modify(|_r, w| w.pb3().set_bit().pb4().clear_bit());
+    loop {
+        peripherals.PORTB.pinb.write(|w| w.pb4().set_bit());
+    }
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
