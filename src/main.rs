d6a9fc478af6b270cfe72e0a0fddf72711a9455d
Julian Frimmel <julian@fri-me.de>
2024-10-06 17:42:34 +0200

    Use watchdog/sleep mode to wake-up device every 8s
    This commit makes the device enter a low power mode at the end of every
    loop iteration. Beforehand, the watchdog timer is configured to raise an
    interrupt after ~8s. This will cause the controller to wake up once that
    interrupt is triggered. The ISR itself is empty, as it is just used for
    waking up. Once the device returns from the ISR, the next loop iteration
    starts.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
 
 #[avr_device::entry]
 fn main() -> ! {
@@ -12,9 +13,30 @@ fn main() -> ! {
         .PORTB
         .portb
         .modify(|_r, w| w.pb3().set_bit().pb4().clear_bit());
+
+    // configure sleep mode to be power down.
+    peripherals.CPU.mcucr.write(|w| w.sm().pdown());
     loop {
         peripherals.PORTB.pinb.write(|w| w.pb4().set_bit());
+
+        // configure the watchdog timer to trigger in ~8s
+        peripherals
+            .WDT
+            .wdtcr
+            .write(|w| w.wdie().set_bit().wdph().set_bit().wdpl().cycles_4k_1024k());
+
+        // enter sleep mode, wake-up is triggered by the next watchdog interrupt
+        peripherals.CPU.mcucr.modify(|_r, w| w.se().set_bit());
+        // SAFETY: this function is not called during `interrupt::free`
+        unsafe { avr_device::interrupt::enable() };
+        avr_device::asm::sleep();
+        peripherals.CPU.mcucr.modify(|_r, w| w.se().clear_bit());
     }
+}
+
+#[avr_device::interrupt(attiny85)]
+fn WDT() {
+    // deliberately empty, just used for waking up the device.
 }
 
 #[panic_handler]
