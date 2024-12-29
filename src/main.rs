2f5bde1567fcf9f89fab65581d41a8dc31acb0aa
Julian Frimmel <julian@fri-me.de>
2024-12-29 23:21:22 +0100

    Fail compilation if a panic could possibly reached

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@
 
 #[avr_device::entry]
 fn main() -> ! {
+    // SAFETY: this is the first an only time, the peripherals are taken.
+    // Normally, this would be done via the safe `take()`-function, but this
+    // introduces a "possible panic" into the code with additional code being
+    // generated. Therefore, this `unsafe`-function is used.
     let peripherals = unsafe { avr_device::attiny85::Peripherals::steal() };
     peripherals
         .PORTB
@@ -39,7 +43,26 @@ fn WDT() {
     // deliberately empty, just used for waking up the device.
 }
 
+/// The panic handler of the application.
+///
+/// Normally, one would fill this panic handler with a reset, a flashing LED or
+/// similar. But this project aims to avoid using panics altogether. Therefore
+/// this panic handler should (in theory) not be necessary as it is never called
+/// by Rust. But: the [`core`]-crate requires the function to be provided by a
+/// `![no_std]`-binary. Therefore this function contains a body, that will not
+/// compile (in reality: will not link) the application due to a missing symbol.
+/// If there is any (potentially) reachable panic (i.e. one, that the optimizer
+/// could not get rid of), an error like this is shown to the user:
+/// ```console
+/// src/main.rs:63:(.text.rust_begin_unwind+0x0): undefined reference to `a_panic_is_reachable'
+/// ```
+/// This indicates, that there is a possibly panicking call and shows even the
+/// line number (63 in this case).
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
+    extern "C" {
+        fn a_panic_is_reachable() -> !;
+    }
+    // SAFETY: if this would be reachable, the code would not compile
+    unsafe { a_panic_is_reachable() };
 }
