547bc5d113972f76d76b8dd98fb2bb6c1ff4e186
Julian Frimmel <julian@fri-me.de>
2024-12-30 13:17:24 +0100

    Fade the leds in and out linearly
    This commit introduces a fade-out complementing the pre-existing fade-in
    with both being linearized for the human eye. This is done using a log-
    table tuned for perceived linearity of the brightness.
    
    The commit also makes sure, that the system clock is shut off so that
    the long delays between the fadings use minimal energy.

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,36 @@ fn main() -> ! {
         .write(|w| w.wgm0().pwm_fast().com0a().match_clear());
     peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
 
-    // configure sleep mode to be idle (since PWM is not working in power down).
-    peripherals.CPU.mcucr.write(|w| w.sm().idle());
+    // https://www.mikrocontroller.net/articles/LED-Fading
+    const LINEARIZATION: [u8; 32] = [
+        0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23, 27, 32, 38, 45, 54, 64, 76, 91,
+        108, 128, 152, 181, 215, 255,
+    ];
+
     loop {
-        for duty_cycle in 0..=255 {
-            peripherals.TC0.ocr0a.write(|w| w.bits(duty_cycle));
-            power::sleep_for::<2>(&mut peripherals.CPU, &mut peripherals.WDT);
+        // configure sleep mode to be idle (since PWM is not working in power down).
+        peripherals.CPU.mcucr.write(|w| w.sm().idle());
+        peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
+        for duty_cycle in &LINEARIZATION {
+            peripherals.TC0.ocr0a.write(|w| w.bits(*duty_cycle));
+            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT);
         }
+
+        peripherals.TC0.tccr0b.write(|w| w.cs0().no_clock());
+        peripherals.CPU.mcucr.write(|w| w.sm().pdown());
+        power::sleep_for::<256>(&mut peripherals.CPU, &mut peripherals.WDT);
+
+        // configure sleep mode to be idle (since PWM is not working in power down).
+        peripherals.CPU.mcucr.write(|w| w.sm().idle());
+        peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
+        for duty_cycle in LINEARIZATION.iter().rev() {
+            peripherals.TC0.ocr0a.write(|w| w.bits(*duty_cycle));
+            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT);
+        }
+
+        peripherals.TC0.tccr0b.write(|w| w.cs0().no_clock());
+        peripherals.CPU.mcucr.write(|w| w.sm().pdown());
+        power::sleep_for::<256>(&mut peripherals.CPU, &mut peripherals.WDT);
     }
 }
 
