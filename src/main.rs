00667357e6f0dbeb71a5901b260e33eafc64a434
Julian Frimmel <julian@fri-me.de>
2024-12-30 14:22:48 +0100

    Extract the timer0 configuration into own module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![feature(abi_avr_interrupt)]
 
 mod power;
+mod timer0;
 
 #[avr_device::entry]
 fn main() -> ! {
@@ -15,42 +16,32 @@ fn main() -> ! {
     power::divide_system_clock_by::<256>(&mut peripherals.CPU); // 8MHz/256≈31kHz
     power::disable_unused_peripherals(&mut peripherals.CPU);
 
-    peripherals.PORTB.ddrb.write(|w| w.pb0().set_bit());
-
-    peripherals
-        .TC0
-        .tccr0a
-        .write(|w| w.wgm0().pwm_fast().com0a().match_clear());
-    peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
-
     // https://www.mikrocontroller.net/articles/LED-Fading
     const LINEARIZATION: [u8; 32] = [
         0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23, 27, 32, 38, 45, 54, 64, 76, 91,
         108, 128, 152, 181, 215, 255,
     ];
 
+    let mut timer = timer0::Timer0::new(peripherals.TC0, &mut peripherals.PORTB);
+
     loop {
         // configure sleep mode to be idle (since PWM is not working in power down).
         peripherals.CPU.mcucr.write(|w| w.sm().idle());
-        peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
-        for duty_cycle in &LINEARIZATION {
-            peripherals.TC0.ocr0a.write(|w| w.bits(*duty_cycle));
-            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT);
-        }
+        timer.pwm(&LINEARIZATION, || {
+            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT)
+        });
 
-        peripherals.TC0.tccr0b.write(|w| w.cs0().no_clock());
+        timer.halt();
         peripherals.CPU.mcucr.write(|w| w.sm().pdown());
         power::sleep_for::<256>(&mut peripherals.CPU, &mut peripherals.WDT);
 
         // configure sleep mode to be idle (since PWM is not working in power down).
         peripherals.CPU.mcucr.write(|w| w.sm().idle());
-        peripherals.TC0.tccr0b.write(|w| w.cs0().direct());
-        for duty_cycle in LINEARIZATION.iter().rev() {
-            peripherals.TC0.ocr0a.write(|w| w.bits(*duty_cycle));
-            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT);
-        }
+        timer.pwm(LINEARIZATION.iter().rev(), || {
+            power::sleep_for::<4>(&mut peripherals.CPU, &mut peripherals.WDT)
+        });
+        timer.halt();
 
-        peripherals.TC0.tccr0b.write(|w| w.cs0().no_clock());
         peripherals.CPU.mcucr.write(|w| w.sm().pdown());
         power::sleep_for::<256>(&mut peripherals.CPU, &mut peripherals.WDT);
     }
---
+++ src/timer0.rs
@@ -0,0 +1,40 @@
+//! Configuration of the Timer/counter 0 as a PWM generator or timebase.
+use avr_device::attiny85::{PORTB, TC0};
+
+/// Access to the 8bit timer/counter 0 peripheral.
+pub struct Timer0(TC0);
+impl Timer0 {
+    /// Create a new [`Timer0`] from a raw register block. This sets the PWM-pin
+    /// PB0 as an output pin as well.
+    pub fn new(registers: TC0, portb: &mut PORTB) -> Self {
+        portb.ddrb.modify(|_, w| w.pb0().set_bit());
+
+        registers
+            .tccr0a
+            .write(|w| w.wgm0().pwm_fast().com0a().match_clear());
+        registers.tccr0b.write(|w| w.cs0().no_clock());
+        Self(registers)
+    }
+
+    /// Perform a PWM sequence with a delay between each duty cycle.
+    pub fn pwm<'a, I: IntoIterator<Item = &'a u8>, F: FnMut()>(
+        &mut self,
+        duty_cycles: I,
+        mut delay: F,
+    ) {
+        self.0
+            .tccr0a
+            .write(|w| w.wgm0().pwm_fast().com0a().match_clear());
+        self.0.tccr0b.write(|w| w.cs0().direct());
+
+        for duty_cycle in duty_cycles {
+            self.0.ocr0a.write(|w| w.bits(*duty_cycle));
+            delay();
+        }
+    }
+
+    // Stop the timer, preserving the previous last PWM output value.
+    pub fn halt(&mut self) {
+        self.0.tccr0b.write(|w| w.cs0().no_clock());
+    }
+}
